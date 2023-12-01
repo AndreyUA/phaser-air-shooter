263ed8438760914175911c2814795526ec4da3c6
AndreyUA <infernalsteel666@gmail.com>
2023-12-01 23:13:34 +0200

    inited phaser instance

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
 import "./style.css";
 
-console.log("project started");
+import * as Phaser from "phaser";
+
+import { preload } from "./preload";
+import { create } from "./create";
+import { update } from "./update";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload,
+    create,
+    update,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+};
+
+const game = new Phaser.Game(config);
