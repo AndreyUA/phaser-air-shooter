9fded35caa49d0daa0144b4beeac534dd198fb53
AndreyUA <infernalsteel666@gmail.com>
2023-12-04 23:26:11 +0200

    implemented basic logic for rocket fire

--- src/update.ts
+++ src/update.ts
@@ -1,6 +1,23 @@
 import { cursors, player } from "./create";
 
+let timer: number | null = null;
+const RELOAD_TIME = 1_500;
+
 export function update() {
+  // ! Rocket fire
+  // TODO: add rocket here
+  if (Phaser.Input.Keyboard.JustDown(cursors.space)) {
+    if (!timer) {
+      timer = Date.now();
+      console.log("FIRE !!! ", new Date());
+    }
+
+    if (Date.now() - timer > RELOAD_TIME) {
+      timer = Date.now();
+      console.log("FIRE !!! ", new Date());
+    }
+  }
+
   // ! Move left
   if (cursors.left.isDown) {
     player.setVelocityX(-360);
