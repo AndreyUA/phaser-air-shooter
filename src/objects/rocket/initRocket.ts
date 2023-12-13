6f123cd1e8bb321fba178ea4876a4dea05e4a63f
AndreyUA <infernalsteel666@gmail.com>
2023-12-13 12:53:39 +0200

    implemented rocket fly

--- src/objects/rocket/initRocket.ts
+++ src/objects/rocket/initRocket.ts
@@ -1,5 +1,7 @@
 import * as Phaser from "phaser";
+
 import { GameObject } from "../../types/gameObjects";
+import { ScreenResolution } from "../../types/screenResolution";
 
 export function initRocket(
   this: Phaser.Scene,
@@ -11,4 +13,15 @@ export function initRocket(
     playerPositionY,
     GameObject.ROCKET
   );
+
+  rocket.body.setGravityY(-10);
+  rocket.body.setVelocityY(ScreenResolution.HEIGHT * -1.2);
+  rocket.setDepth(0);
+
+  const interval = setInterval(() => {
+    if (rocket.y < 0) {
+      clearInterval(interval);
+      rocket.destroy();
+    }
+  }, 100);
 }
