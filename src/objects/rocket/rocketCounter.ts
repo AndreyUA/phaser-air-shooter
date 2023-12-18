3e26e54842930f7f36dd721b10f02b458fc8b7f0
AndreyUA <infernalsteel666@gmail.com>
2023-12-18 10:42:51 +0200

    improved logic for rocket counter calculation

--- src/objects/rocket/rocketCounter.ts
+++ src/objects/rocket/rocketCounter.ts
@@ -1,9 +1,17 @@
-let rocketCounter: number = 6;
+let rocketCounter: number = 9;
 
 export const getRocketCounter = (): number => rocketCounter;
 export const incrementRocketCounter = (): void => {
+  if (rocketCounter === 9) {
+    return;
+  }
+
   rocketCounter++;
 };
 export const decrementRocketCounter = (): void => {
+  if (rocketCounter === 0) {
+    return;
+  }
+
   rocketCounter--;
 };
