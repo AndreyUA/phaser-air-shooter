cad9cddcfc0735e38deb2781ad6e5ea09d2fd234
AndreyUA <infernalsteel666@gmail.com>
2023-12-19 09:55:31 +0200

    created calculateRandomValueBetween0And100 util

--- src/objects/rocket/initAdditionalRocket.ts
+++ src/objects/rocket/initAdditionalRocket.ts
@@ -1,16 +1,13 @@
-import * as Phaser from "phaser";
-
+import { calculateRandomValueBetween0And100 } from "../../utils/calculateRandomValueBetween0And100";
 import { getRocketCounter } from "./rocketCounter";
-
-const calculateRandomValue = () => Phaser.Math.Between(0, 100);
 
 // TODO: add additional rocket to the game and make it possible to collect it
 export function initAdditionalRocket(): void {
   // ! 5% chance to add rocket if 8 rocket left
   if (
     getRocketCounter() === 8 &&
-    calculateRandomValue() > 55 &&
-    calculateRandomValue() <= 60
+    calculateRandomValueBetween0And100() > 55 &&
+    calculateRandomValueBetween0And100() <= 60
   ) {
     console.log("its time to add rocket!");
   }
@@ -18,8 +15,8 @@ export function initAdditionalRocket(): void {
   // ! 10% chance to add rocket if 7 rocket left
   if (
     getRocketCounter() === 7 &&
-    calculateRandomValue() > 1 &&
-    calculateRandomValue() <= 11
+    calculateRandomValueBetween0And100() > 1 &&
+    calculateRandomValueBetween0And100() <= 11
   ) {
     console.log("its time to add rocket!");
   }
@@ -27,8 +24,8 @@ export function initAdditionalRocket(): void {
   // ! 15% chance to add rocket if 6 rocket left
   if (
     getRocketCounter() === 6 &&
-    calculateRandomValue() > 30 &&
-    calculateRandomValue() <= 45
+    calculateRandomValueBetween0And100() > 30 &&
+    calculateRandomValueBetween0And100() <= 45
   ) {
     console.log("its time to add rocket!");
   }
@@ -36,8 +33,8 @@ export function initAdditionalRocket(): void {
   // ! 20% chance to add rocket if 5 rocket left
   if (
     getRocketCounter() === 5 &&
-    calculateRandomValue() > 60 &&
-    calculateRandomValue() <= 80
+    calculateRandomValueBetween0And100() > 60 &&
+    calculateRandomValueBetween0And100() <= 80
   ) {
     console.log("its time to add rocket!");
   }
@@ -45,8 +42,8 @@ export function initAdditionalRocket(): void {
   // ! 25% chance to add rocket if 4 rocket left
   if (
     getRocketCounter() === 4 &&
-    calculateRandomValue() > 70 &&
-    calculateRandomValue() <= 95
+    calculateRandomValueBetween0And100() > 70 &&
+    calculateRandomValueBetween0And100() <= 95
   ) {
     console.log("its time to add rocket!");
   }
@@ -54,8 +51,8 @@ export function initAdditionalRocket(): void {
   // ! 30% chance to add rocket if 3 rocket left
   if (
     getRocketCounter() === 3 &&
-    calculateRandomValue() > 50 &&
-    calculateRandomValue() <= 80
+    calculateRandomValueBetween0And100() > 50 &&
+    calculateRandomValueBetween0And100() <= 80
   ) {
     console.log("its time to add rocket!");
   }
@@ -63,8 +60,8 @@ export function initAdditionalRocket(): void {
   // ! 35% chance to add rocket if 2 rocket left
   if (
     getRocketCounter() === 2 &&
-    calculateRandomValue() > 64 &&
-    calculateRandomValue() <= 99
+    calculateRandomValueBetween0And100() > 64 &&
+    calculateRandomValueBetween0And100() <= 99
   ) {
     console.log("its time to add rocket!");
   }
@@ -72,8 +69,8 @@ export function initAdditionalRocket(): void {
   // ! 40% chance to add rocket if 1 rocket left
   if (
     getRocketCounter() === 1 &&
-    calculateRandomValue() > 40 &&
-    calculateRandomValue() <= 80
+    calculateRandomValueBetween0And100() > 40 &&
+    calculateRandomValueBetween0And100() <= 80
   ) {
     console.log("its time to add rocket!");
   }
@@ -81,8 +78,8 @@ export function initAdditionalRocket(): void {
   // ! 45% chance to add rocket if 0 rocket left
   if (
     getRocketCounter() === 0 &&
-    calculateRandomValue() > 55 &&
-    calculateRandomValue() <= 100
+    calculateRandomValueBetween0And100() > 55 &&
+    calculateRandomValueBetween0And100() <= 100
   ) {
     console.log("its time to add rocket!");
   }
---
+++ src/utils/calculateRandomValueBetween0And100.ts
@@ -0,0 +1,4 @@
+import * as Phaser from "phaser";
+
+export const calculateRandomValueBetween0And100 = () =>
+  Phaser.Math.Between(0, 100);
